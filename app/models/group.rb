b9ac67406c8ab11d4c88ae327b7d92ce7a4f9bab
Stephen Gregory <stejgregory@gmail.com>
2016-11-22 13:10:33 +0000

    Deleted groups controller spec, associated Groups with User

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,5 @@
 class Group < ApplicationRecord
+  has_many :users
+  belongs_to :user
+  validates :name, length: { minimum: 1 }, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122102716) do
+ActiveRecord::Schema.define(version: 20161122125329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.string   "members"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "training_sessions", force: :cascade do |t|
     t.string   "name"
