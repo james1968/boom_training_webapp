4a0b02ebdf592a99e30438ef6a78980e28e7afcd
Stephen Gregory <stejgregory@gmail.com>
2016-11-22 15:58:53 +0000

    Groups working and passing, need to create better member find option in new groups

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,13 +3,10 @@ require 'rails_helper'
 describe Group, type: :model do
 	let(:user1) { User.create email: 'test1@test.com', password: '123456' }
 	let(:user2) { User.create email: 'test2@test.com', password: '654321' }
-
-	describe 'making our first group' do
-		it 'should allow us add users' do
-			group = Group.create(name: "Team Boom")
-			group.add_user(user1)
-			expect(group.show_members).to eq user1
-		end
-	end
+ it 'is not a valid group unless its name is unique' do
+ 	Group.create(name: "Team Boom", description: "Everything is awesome", user_id: user1.id)
+	group2 = Group.new(name: "Team Boom", description: "Everything is awesome", user_id: user2.id)
+	expect(group2).to have(1).error_on(:name)
+ end
 
 end
