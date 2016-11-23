f4a468063949c52ffa96fd0edeab8451316daaae
alexanderwjrussell <awjrussell@live.co.uk>
2016-11-23 11:42:49 +0000

    Formatted the views 1) Added links to add new group and view groups 2) Modified the migrations to include descriptions rather than members

--- db/migrate/20161122125329_create_groups.rb
+++ db/migrate/20161122125329_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
       t.string :name
-      t.string :members
+      t.string :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122140002) do
+ActiveRecord::Schema.define(version: 20161122150501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
-    t.string   "members"
+    t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.text     "description"
     t.integer  "user_id"
   end
 
