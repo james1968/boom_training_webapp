dd33f9524f4e1f1484b5a47f23c02865ee952460
alexanderwjrussell <awjrussell@live.co.uk>
2016-11-25 17:29:54 +0000

    Minor modifications 1) User picture size

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,6 +3,7 @@ class CalendarsController < ApplicationController
   def index
   	@user = current_user
     @training_sessions = TrainingSession.all
+    @training_session = TrainingSession.where(user_id: current_user.id)
   end
 
 end
