ab46f32e0739655de4bf8125493dcfa1b6a46565
alexanderwjrussell <awjrussell@live.co.uk>
2016-11-25 09:28:16 +0000

    First commit for new branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,12 @@ class User < ApplicationRecord
   has_many :friendships, class_name: "Friendship",
   											 foreign_key: "first_user_id",
   											 dependent: :destroy
-  
+
   has_many :friendship_ones, :class_name => 'Friendship', :foreign_key => :second_user_id
 	has_many :friend_ones, through: :friendship_ones, source: :first_user
 	has_many :friendship_twos, :class_name => 'Friendship', :foreign_key => :first_user_id
 	has_many :friend_twos, through: :friendship_twos, source: :second_user
+  
   has_many :groups
 
   #has_many :friends, through: :friendships, source: :second_user
