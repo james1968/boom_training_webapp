454dbd37eb8eb087e931a55a2e0f9956b08fda30
alexanderwjrussell <awjrussell@live.co.uk>
2016-11-25 15:08:43 +0000

    User Rating 1) User profile now displays an average rating of sessions 2) Modified usr view and controller 3) Created a new method in User to calculate average

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    @training_session = TrainingSession.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161125110711) do
+ActiveRecord::Schema.define(version: 20161125150043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,10 +27,9 @@ ActiveRecord::Schema.define(version: 20161125110711) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
-    t.string   "members"
+    t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.text     "description"
     t.integer  "user_id"
   end
 
@@ -75,6 +73,7 @@ ActiveRecord::Schema.define(version: 20161125110711) do
     t.datetime "image_updated_at"
     t.string   "username"
     t.string   "postcode"
+    t.integer  "rating"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
