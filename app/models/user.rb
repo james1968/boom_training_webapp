6de2b00163a1b9250addd3d3f55f1476247049eb
Stephen Gregory <stejgregory@gmail.com>
2016-11-25 14:30:19 +0000

    Changed image acceptance for any image type

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,8 @@ class User < ApplicationRecord
   has_many :groups
 
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "400x400" }
-    validates_attachment_content_type :image, :content_type => ['image/jpg', 'image/png']
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  # validates_attachment_content_type :image, :content_type => ['image/jpg', 'image/png']
 
   #has_many :friends, through: :friendships, source: :second_user
 
